e20259028befa49149c0abf5dfec8736b79026e4
Poojetha <dkpooji247@gmail.com>
2026-01-21 11:59:59 +0530

    Update Account not found Exception

--- Financial_Transcation_Record_System/src/com/wipro/ftrs/util/AccountNotFoundException.java
+++ Financial_Transcation_Record_System/src/com/wipro/ftrs/util/AccountNotFoundException.java
@@ -1,7 +1,13 @@
 package com.wipro.ftrs.util;
 
 public class AccountNotFoundException extends Exception {
+
     public AccountNotFoundException(String message) {
         super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountNotFoundException: " + getMessage();
     }
 }
