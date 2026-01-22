11a5bdc27461ef4e96acf0d4a9e9698885079c1a
Poojetha <dkpooji247@gmail.com>
2026-01-22 09:24:36 +0530

    Update Insufficient Balance

--- Financial_Transcation_Record_System/src/com/wipro/ftrs/util/InsufficientBalanceException.java
+++ Financial_Transcation_Record_System/src/com/wipro/ftrs/util/InsufficientBalanceException.java
@@ -1,7 +1,13 @@
 package com.wipro.ftrs.util;
 
 public class InsufficientBalanceException extends Exception {
+
     public InsufficientBalanceException(String message) {
         super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "InsufficientBalanceException: " + getMessage();
     }
 }
