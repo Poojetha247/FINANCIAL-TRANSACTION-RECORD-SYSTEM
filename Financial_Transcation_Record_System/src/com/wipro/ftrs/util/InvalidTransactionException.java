ddeb045d3592eaea5004b27deea174f841bfb7cb
Poojetha <dkpooji247@gmail.com>
2026-01-22 09:22:44 +0530

    update invalid transaction

--- Financial_Transcation_Record_System/src/com/wipro/ftrs/util/InvalidTransactionException.java
+++ Financial_Transcation_Record_System/src/com/wipro/ftrs/util/InvalidTransactionException.java
@@ -1,7 +1,13 @@
 package com.wipro.ftrs.util;
 
 public class InvalidTransactionException extends Exception {
+
     public InvalidTransactionException(String message) {
         super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "InvalidTransactionException: " + getMessage();
     }
 }
